d27c6c8c361bd8f8a64bc0e6076da9bc8e79621f
Alec Benson <alecbenson21@gmail.com>
2022-01-11 17:48:54 -0700

    Got parser body generation working for grammars

--- experiments/translatorGen/grammarParser.py
+++ experiments/translatorGen/grammarParser.py
@@ -1,8 +1,11 @@
 from tokenize import NAME, ENDMARKER, STRING, NEWLINE
 from std import Parser, Node
-
 class GrammarParser(Parser):
-
+    nameCount = 0
+    body = ""
+    currentRule = ""
+    andPart = []
+    andReturns = []
     def start(self):
         if r := self.rules():
             pos = self.mark()
@@ -21,18 +24,26 @@ class GrammarParser(Parser):
         return None
 
     def rule(self):
+        #each rule should produce a function for the Parser, 
+        #and a node class
         pos = self.mark()
-        if (n := self.expect(NAME) 
+        if ((n := self.expect(NAME)) 
             and self.expect(":") 
             and (a := self.alts()) 
-            and self.expect("{") 
-            and (t := self.alt()) 
-            and self.expect("}") 
             and self.expect(NEWLINE)
         ):
-            return Node("rule", [n, a, t])
+            self.body += f"""
+            def {n.string}(self):
+                {self.currentRule}
+                return None
+            """
+            self.currentRule = ""
+            return Node("rule", [n, a])
         self.reset(pos)
         return None
+
+    def translation(self):
+        return True
 
     def alts(self):
         if a := self.alt():
@@ -46,6 +57,14 @@ class GrammarParser(Parser):
 
     def alt(self):
         if i := self.items():
+            self.currentRule += f"""
+                pos = self.mark()
+                if({" and ".join(self.andPart)}):
+                    return [{", ".join(self.andReturns)}]
+                self.reset(pos)
+            """
+            self.andPart = []
+            self.andReturns = []
             return Node('alt', [i])
         return None
 
@@ -53,16 +72,21 @@ class GrammarParser(Parser):
         if i := self.item():
             pos = self.mark()
             if iSin := self.items():
-                return Node('items', [i, iSin])
+                return Node("items", [i, iSin])
             self.reset(pos)
-            return Node('items', [i])
+            return Node("items", [i])
         return None
     
     def item(self):
         pos = self.mark()
         if n := self.expect(NAME):
-            return Node('item', [n])
+            self.andPart.append(f"(n{self.nameCount} := self.expect({n.string}))" if n.string.isupper() else f"(n{self.nameCount} := self.{n.string}())")
+            self.andReturns.append(f"({n.string}, n{self.nameCount})")
+            self.nameCount += 1
+            return n
         self.reset(pos)
         if s := self.expect(STRING):
-            return Node('item', [s])
+            self.andPart.append(f"self.expect({s.string})")
+            self.nameCount += 1
+            return s
         return None
