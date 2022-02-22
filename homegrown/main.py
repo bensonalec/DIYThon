6f2d8bb1e0b7c39b7ccbb08e95ec9fbd407a7a45
Alec Benson <alecbenson21@gmail.com>
2022-02-22 16:20:40 -0700

    Heavily cleaned the repo up

--- homegrown/main.py
+++
@@ -1,8 +0,0 @@
-from Tokenizer import Tokenizer
-from toyParser import ToyParser
-from tokenize import generate_tokens
-
-with open("sampleInput.txt") as fi:
-    tokenGen = generate_tokens(fi.read)
-    p = ToyParser(Tokenizer(tokenGen))
-    print(p.start())
